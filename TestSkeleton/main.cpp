6e96ed8d3d48ce7d376fbb56ac0ca82bd65a1bdf
yutarokinoshita <65150857+yutarokinoshita@users.noreply.github.com>
2021-05-19 20:17:00 +0900

    Update main.cpp

--- TestSkeleton/main.cpp
+++ TestSkeleton/main.cpp
@@ -3,7 +3,7 @@
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance,_In_opt_ HINSTANCE hPrewInstance,_In_ LPSTR lpCmdLine,_In_ int mShowCmd) 
 {
-	DxLib::SetWindowText("1916013_–Ø‰º—S‘¾˜N");
+	DxLib::SetWindowText("1916013_æœ¨ä¸‹ç¥å¤ªæœ—");
 	ChangeWindowMode(true);
 	DxLib_Init();
 	SetDrawScreen(DX_SCREEN_BACK);
