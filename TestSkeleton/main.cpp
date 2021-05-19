5c6e532a228bec3426eb1881e92cafd3d6dee80c
yutarokinoshita <1916013@s.asojuku.ac.jp>
2021-05-19 20:27:20 +0900

    データ更新

--- TestSkeleton/main.cpp
+++ TestSkeleton/main.cpp
@@ -3,6 +3,7 @@
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance,_In_opt_ HINSTANCE hPrewInstance,_In_ LPSTR lpCmdLine,_In_ int mShowCmd) 
 {
+
 	DxLib::SetWindowText("1916013_–Ø‰º—S‘¾˜N");
 	ChangeWindowMode(true);
 	DxLib_Init();
