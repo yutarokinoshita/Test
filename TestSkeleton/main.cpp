fe20180c1b5a79273faf3dd36451b0ae7ea4f2ca
yutarokinoshita <1916013@s.asojuku.ac.jp>
2021-05-19 15:58:38 +0900

    スケルトン

--- TestSkeleton/main.cpp
+++ TestSkeleton/main.cpp
@@ -1,18 +1,10 @@
 #include <DxLib.h>
 #include <string>
-#include <algorithm>
 
-#define ABC
-using namespace std;
+int WINAPI WinMain(_In_ HINSTANCE hInstance,_In_opt_ HINSTANCE hPrewInstance,_In_ LPSTR lpCmdLine,_In_ int mShowCmd) 
+{
 
-int WINAPI WinMain(_In_ HINSTANCE hInstance,_In_opt_ HINSTANCE hPrewInstance,_In_ LPSTR lpCmdLine,_In_ int mShowCmd) {
-	
-	int lval = 3;
-	int rval = 5;
-	auto minval = std::min(lval, rval);
-	auto maxval = std::max(lval, rval);
-
-	DxLib::SetWindowText("サンプルプログラム");
+	DxLib::SetWindowText("1916013_木下祐太朗");
 	ChangeWindowMode(true);
 	DxLib_Init();
 	SetDrawScreen(DX_SCREEN_BACK);
